8b5c0a7b852fa091b97e47aeefb90657d1eaea46
Märt Lehtmets <mart.lehtmets>
2018-11-29 14:26:00 +0200

    commit2

--- Pealeht.xaml.cs
+++ Pealeht.xaml.cs
@@ -26,5 +26,10 @@ namespace grupitoo
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Tunniplaan));
+        }
     }
 }
